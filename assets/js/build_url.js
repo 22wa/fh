e6d61cddbf138a9abff5d1a08c35850c430bc5e1
22wa <133514201+22wa@users.noreply.github.com>
2023-05-15 06:29:32 +0000

    Add files via upload

---
+++ assets/js/build_url.js
@@ -0,0 +1,17 @@
+function build_url(){
+    var url = encodeURI(decodeURI(document.querySelector('#url').value));
+    if (url === "" || url.indexOf("http") === -1){
+        document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
+    }
+    else {
+        url = "https://softfabu.github.io/fanghong/" + "api/?url=" + url;
+        document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
+    }
+}
+
+
+
+/**
+*url = document.location.href + "api/?url=" + url;
+*/
+
