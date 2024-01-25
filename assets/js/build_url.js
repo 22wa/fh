2c9e4b87e060888b96e7cace4f5f9bafcebe25e0
22wa <133514201+22wa@users.noreply.github.com>
2024-01-25 14:08:02 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -4,7 +4,7 @@ function build_url(){
         document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
     }
     else {
-        url = "https://22s.github.io/fh/" + "api/?url=" + url;
+        url = "https://22wa.github.io/fh/" + "api/?url=" + url;
         document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
     }
 }
